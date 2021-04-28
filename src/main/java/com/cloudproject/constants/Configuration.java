0dd846d26e5531be201beb8f9b2391f0af9cbebb
Darshil <dshah27@asu.edu>
2021-04-28 16:24:56 +0530

    Final Commit
    Removed Credentials

--- src/main/java/com/cloudproject/constants/Configuration.java
+++ src/main/java/com/cloudproject/constants/Configuration.java
@@ -2,15 +2,15 @@ package com.cloudproject.constants;
 
 public class Configuration {
 	private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-	private static final String DATABASE_NAME = "bookbank_db";
-	private static final String INSTANCE_CONNECTION_NAME = "book-311509:us-central1:bookbankservicedb";
+	private static final String DATABASE_NAME = "*****";
+	private static final String INSTANCE_CONNECTION_NAME = "*****";
 	private static final String MYSQL_USER_NAME = "root"; 
 	private static final String MYSQL_USER_PASSWORD = "root"; 
-	private static final String PROJECT_ID = "book-311509";
-	private static final String BUCKET_NAME = "cse546bookbankbucket";
-	private static final String SENDER_ID = "cse546.spring2021@gmail.com";
+	private static final String PROJECT_ID = "*****";
+	private static final String BUCKET_NAME = "*****";
+	private static final String SENDER_ID = "****;
 	private static final String HOST_ID = "smtp.gmail.com";
-	private static final String PASSWORD = "Admin@123";
+	private static final String PASSWORD = "*****";
 	
 	public static String getProjectId() {
 		return PROJECT_ID;
